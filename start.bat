8f6123199aebbcdb19b34d0e971a969f220d8470
Bolohonov <bolohonov@list.ru>
2023-04-19 14:25:25 +0400

    Initial commit

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo OFF
-start C:\Users\NoteBook\.jdks\corretto-17.0.4.1\bin\java.exe -jar target/evrika-test-app-0.0.1-SNAPSHOT.jar
+start java -jar target/evrika-test-app-0.0.1-SNAPSHOT.jar
 cd ui
 start npm run serve -- --port 8081
 timeout 10
